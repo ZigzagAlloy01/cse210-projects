b526b16511d970352ba42b407df11ef594328f8d
ZigzagAlloy01 <ivanblued@gmail.com>
2023-10-17 22:11:23 -0600

    Actualizacion el 17 de octubre de 2023

--- prove/Develop03/Scripture.cs
+++ prove/Develop03/Scripture.cs
@@ -1,16 +1,29 @@
+using System.ComponentModel.DataAnnotations;
+
 public class Scripture 
 {
-    private string[] _wordsInScripture;
-
+    public string _file;
+    public List<string> _listOfScriptures;
+    public List<string> _election;
+    private List<string> _lettersInScripture = new List<string>();
+    public Scripture() {}
     public void LoadScripture()  {
-
+        Random rnd = new Random();
+        string anyScripture = _listOfScriptures[rnd.Next(6)];
+        Console.WriteLine(anyScripture);
+        _election.Add(anyScripture);
+        }
+    public void GetScripture() {
+        string elements = _election[0];
+        char [] letters = elements.ToCharArray();
+        foreach (char letter in letters) {
+            _lettersInScripture.Add((letter.ToString()));
+        }
     }
-    public string[] GetScripture() {
-        
-    }
-
-    public string GetBlackedOutScripture(List<int> blackedOutWords) {
-
+    public string GetBlackedOutScripture() {
+        Random rnd = new Random();
+        string anyLetter = _lettersInScripture[rnd.Next(_lettersInScripture.Count)];
+        return anyLetter;
     }
 
 }
