7f1965e2e31cce0c6be3ad2170c255b6db4e3a05
ZigzagAlloy01 <ivanblued2@gmail.com>
2023-10-28 23:49:10 -0700

    New learning activity results

---
+++ prepare/Learning04/Assignment.cs
@@ -0,0 +1,25 @@
+public class Assignment
+{
+    private string _student;
+    private string _topic;
+
+    public Assignment(string student, string topic)
+    {
+        _student = student;
+        _topic = topic;
+    }
+    public string GetStudent()
+    {
+        return _student;
+    }
+
+    public string GetTopic()
+    {
+        return _topic;
+    }
+
+    public string GetSummary()
+    {
+        return _student + " - " + _topic;
+    }
+}
--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,13 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        Assignment math1 = new Assignment("Samuel Bennett", "Multiplication");
+        Console.WriteLine(math1.GetSummary());
+        MathAssignment math2 = new MathAssignment("Roberto Rodriguez", "Fractions", "7.3", "8-19");
+        Console.WriteLine(math2.GetSummary());
+        Console.WriteLine(math2.GetHomeworkList());
+        WritingAssignment write1 = new WritingAssignment("Mary Waters", "European History", "The Causes of World War II");
+        Console.WriteLine(write1.GetSummary());
+        Console.WriteLine(write1.GetWritingInformation());
     }
 }
